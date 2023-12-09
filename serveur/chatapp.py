d9795e76017db5be50c857b768978a28daf5fc31
Ykari68 <assens.assens@gmail.com>
2023-12-09 10:47:54 +0100

    ça marche (un peu)

--- serveur/chatapp.py
+++ serveur/chatapp.py
@@ -1,49 +1,74 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLineEdit, QPushButton, QTextBrowser, QInputDialog
+from PyQt5.QtCore import QThread, pyqtSignal
 import socket
-import threading
 
-def com():
-    while True:
-        message = input("Vous: ")
-        client_socket.send(message.encode())
-        if message.lower() == "quit":
-            client_socket.close()
-            break
+class ClientThread(QThread):
+    message_received = pyqtSignal(str)
 
-def receive_messages():
-    while True:
+    def __init__(self, username, password):
+        super().__init__()
+        self.username = username
+        self.password = password
+
+    def run(self):
         try:
-            message = client_socket.recv(1024).decode()
-            print(message)
-        except ConnectionResetError:
-            print("La connexion avec le serveur a été interrompue.")
-            client_socket.close()
-            break
-        except ConnectionAbortedError and OSError:
-            print("Vous avez quitté la discussion.")
-            client_socket.close()
-            break
-        except:
-            client_socket.close()
-            break
+            client_socket = socket.socket()
+            client_socket.connect(('127.0.0.1', 6255))
+            client_socket.send(self.username.encode())
+            client_socket.send(self.password.encode())
 
-server_address = "127.0.0.1" #input("Entrez l'adresse IP du serveur : ")
-server_port = 6255 #int(input("Entrez le numéro de port du serveur : "))
+            while True:
+                message = client_socket.recv(1024).decode()
+                self.message_received.emit(message)
 
-client_socket = socket.socket()
-client_socket.connect((server_address, server_port))
+        except Exception as e:
+            print(f"Error: {e}")
 
-username = input("Entrez votre nom d'utilisateur : ")
-password = input("Entrez votre mot de passe : ")
-client_socket.send(username.encode())
-client_socket.send(password.encode())
+class ChatClient(QWidget):
+    def __init__(self):
+        super().__init__()
 
-com_thread = threading.Thread(target=com)
-receive_thread = threading.Thread(target=receive_messages)
+        self.setWindowTitle("Chat Client")
+        self.setGeometry(100, 100, 400, 300)
 
-com_thread.start()
-receive_thread.start()
+        layout = QVBoxLayout()
 
-com_thread.join()
-receive_thread.join()
+        self.message_browser = QTextBrowser()
+        layout.addWidget(self.message_browser)
 
-client_socket.close()
+        self.message_input = QLineEdit()
+        self.message_input.setPlaceholderText("Type your message here...")
+        layout.addWidget(self.message_input)
+
+        send_button = QPushButton("Send")
+        send_button.clicked.connect(self.send_message)
+        layout.addWidget(send_button)
+
+        self.setLayout(layout)
+
+        self.client_thread = None
+        self.start_client()
+
+    def start_client(self):
+        username, ok1 = QInputDialog.getText(self, 'Username', 'Enter your username:')
+        password, ok2 = QInputDialog.getText(self, 'Password', 'Enter your password:', QLineEdit.Password)
+
+        if ok1 and ok2:
+            self.client_thread = ClientThread(username, password)
+            self.client_thread.message_received.connect(self.update_chat_display)
+            self.client_thread.start()
+
+    def send_message(self):
+        message = self.message_input.text()
+        # Envoie du message au serveur, à implémenter selon le protocole de communication
+        self.message_input.clear()
+
+    def update_chat_display(self, message):
+        self.message_browser.append(message)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    chat_client = ChatClient()
+    chat_client.show()
+    sys.exit(app.exec_())
