8e22571345c90b404dd97d40851e5233f305a2d2
Ykari68 <assens.assens@gmail.com>
2023-12-10 17:50:37 +0100

    client avec gui marche !

--- serveur/client2.py
+++ serveur/client2.py
@@ -6,74 +6,83 @@ from PyQt6 import *
 from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLineEdit, QLabel, QDialog, QComboBox
 from PyQt6.QtCore import Qt, pyqtSignal, pyqtSlot, QThread
 
-class Main(QWidget):
+class Login(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("Compte")
+        self.setWindowTitle("Login")
         self.setGeometry(800, 400, 200, 100)
 
         layout = QVBoxLayout()
 
-        self.username = QLineEdit()
-        self.username.setPlaceholderText("Entrez votre nom d'utilisateur")
-        layout.addWidget(self.username)
+        self.username_saisi = QLineEdit()
+        self.username_saisi.setPlaceholderText("Enter your username")
+        layout.addWidget(self.username_saisi)
 
-        self.password = QLineEdit()
-        self.password.setPlaceholderText("Entrez votre mot de passe")
-        layout.addWidget(self.password)
+        self.password_saisi = QLineEdit()
+        self.password_saisi.setPlaceholderText("Enter your password")
+        layout.addWidget(self.password_saisi)
 
         ok = QPushButton("Ok")
-        ok.clicked.connect(self.send(self.password))
+        ok.clicked.connect(self.start_chat)
         layout.addWidget(ok)
 
         self.setLayout(layout)
-        
+
+    def start_chat(self):
+        username = self.username_saisi.text()
+        password = self.password_saisi.text()
+
+        self.fenêtre = Main(username, password)
+        self.fenêtre.show()
+
+        self.close()
+
+class Main(QWidget):
+    def __init__(self, username, password):
+        super().__init__()
+
         self.ui()
 
-    def send(username, password):
-        socket_thread = SocketThread(username, password)
-        socket_thread.message_recu.connect(self.maj_chat)
-        socket_thread.start()
+        self.socket_thread = SocketThread(username, password)
+        self.socket_thread.message_recu.connect(Main.maj_chat)
+        self.socket_thread.start()
 
     def ui(self):
 
+        layout = QVBoxLayout()
+
         self.setWindowTitle("Chat App")
         self.setGeometry(800, 400, 500, 400)
-
-        layout = QVBoxLayout()
 
         self.message_recu = QLineEdit()
         self.message_recu.setPlaceholderText("")
         layout.addWidget(self.message_recu)
 
-        self.vous = QLabel("Vous:")
+        self.vous = QLabel("You:")
         layout.addWidget(self.vous)
 
-        self.message_saisi = QLineEdit()
-        self.message_saisi.setPlaceholderText("Envoyer un message")
-        layout.addWidget(self.message_saisi)
+        self.message_recu = QLineEdit()
+        self.message_recu.setPlaceholderText("Type a message")
+        layout.addWidget(self.message_recu)
 
-        envoi = QPushButton("Envoyer")
-        envoi.clicked.connect(self.envoi)
-        layout.addWidget(envoi)
-
-        '''compte = QPushButton("Compte")
-        compte.clicked.connect(self.compte)
-        layout.addWidget(compte)'''
+        send_button = QPushButton("Send")
+        send_button.clicked.connect(self.envoi)
+        layout.addWidget(send_button)
 
         self.setLayout(layout)
 
     def envoi(self):
-        message = str(self.message_saisi.text())
+        message = self.message_recu.text()
         self.socket_thread.envoi(message)
-        self.message_saisi.clear()
+        self.message_recu.clear()
 
     @pyqtSlot(str)
     def maj_chat(self, message):
-        self.message_recu = message
+        self.message_recu.setText(message)
 
-class SocketThread(QThread, QWidget):
+
+class SocketThread(QThread):
     message_recu = pyqtSignal(str)
 
     def __init__(self, username, password):
@@ -85,32 +94,28 @@ class SocketThread(QThread, QWidget):
         self.client_socket = socket.socket()
         self.client_socket.connect((self.server_address, self.server_port))
 
+        self.client_socket.send(username.encode())
+        time.sleep(3)
+        self.client_socket.send(password.encode())
 
     def ecoute(self):
-        def receive_messages():
-            while True:
-                try:
-                    message = client_socket.recv(1024).decode()
-                    print(message)
-                except ConnectionResetError:
-                    print("La connexion avec le serveur a été interrompue.")
-                    client_socket.close()
-                    break
-                except ConnectionAbortedError and OSError:
-                    print("Vous avez quitté la discussion.")
-                    client_socket.close()
-                    break
-                except:
-                    client_socket.close()
-                    break
+        while True:
+            try:
+                message = self.client_socket.recv(1024).decode()
+                self.message_recu.emit(message)
+            except (ConnectionResetError, ConnectionAbortedError, OSError):
+                print("Connection error or client exit.")
+                self.client_socket.close()
+                break
+
     def envoi(self, message):
         self.client_socket.send(message.encode('utf-8'))
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    fenetre = Main()
-    fenetre.show()
+    window = Login()
+    window.show()
     sys.exit(app.exec())
 
     
